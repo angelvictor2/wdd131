99c416dbba477c38f7398c994ea127870eae7d26
angelvictor2 <avictorescobedo@byupathway.edu>
2026-01-21 22:01:52 -0600

    week02 info

---
+++ week02/scripts/bom-scripts.js
@@ -0,0 +1,28 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', function(){
+    if (input.value.trim() !== ''){
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        li.textContent = input.value;
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+    }
+    input.focus();
+});
+
+list.addEventListener('click', function (e) {
+    if (e.target.tagName === 'BUTTON') {
+        const li = e.target.parentElement;
+        list.removeChild(li);
+        input.focus();
+    }
+});
+
+
---
+++ week02/scripts/debugging.js
@@ -0,0 +1,15 @@
+const radiusOutput = document.getElementById('radius');
+const areaOutput = document.querySelector('area');
+
+
+const PI = 3.14159;
+
+const radius = 10;
+area = PI * radius * radius;
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
+
+const area = PI * radius * radius;
+
+radiusOutput.textContent = radius;
+areaOutput.textContent = area;
