80fbc7f822c91bacfca1c9d02eeb4e4999a4248b
angelvictor2 <avictorescobedo@byupathway.edu>
2026-01-22 23:44:43 -0600

    Temple Album

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
