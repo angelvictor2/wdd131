e15363b97bcb975fa17e605a6d98b0c51605f7e0
angelvictor2 <avictorescobedo@byupathway.edu>
2026-08-06 20:03:03 -0600

    week 05

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,29 @@
 const products = [
-    { id: "washer3000", name: "Washer 3000" },
-    { id: "dryerpro", name: "Dryer Pro" },
-    { id: "refrigeratorxl", name: "Refrigerator XL" },
-    { id: "dishwasherplus", name: "Dishwasher Plus" }
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0
+    }
 ];
 
 const productSelect = document.querySelector('#product');
